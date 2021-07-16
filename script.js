35213effe4c736dad57625ccb50bc59b9e4623f7
Mattyjaws <jaworski.matthew.93@gmail.com>
2021-07-15 22:28:57 -0700

    Updating js file with full functions and variables.

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var numbers = "0123456789";
+var specials = "!@#$%^&*()";
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -9,6 +14,38 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
+function generatePassword(){
+  
+  var length = window.prompt("How long do you want your password, pick a number between 8 and 128!");
+  console.log(typeof length);
+  
+  if (length < 8 || length > 128) {
+    window.alert("Choose a number betwen 8 and 128!!!!!")
+    return;
+  }
+  
+  var upper = window.confirm("Do you want to use uppercase letters?");
+  var lower = window.confirm("Do you want to use lowercase letters?");
+  var number = window.confirm("Do you want to use numbers?");
+  var special = window.confirm("Do you want to use special symbols?");
+  var passString = "";
+
+  let charInput = length
+  upper ? (charInput += uppercase) : "";
+  lower ? (charInput += lowercase) : "";
+  number ? (charInput += numbers) : "";
+  special ? (charInput += specials) : "";
+
+  for (let i=0; i < length; i++) {
+    var index = Math.floor(Math.random() * charInput.length);
+    var random = charInput[index];
+    passString += random;
+  }
+
+  return passString;
+}
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
